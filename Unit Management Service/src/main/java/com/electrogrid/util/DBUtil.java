720e486fd39b6c15e08ff3c1039512fa0c63c2b5
RashaniTharushika <rashanitharushika99@gmail.com>
2022-04-20 13:50:58 +0530

    Update DBUtil.java

--- Unit Management Service/src/main/java/com/electrogrid/util/DBUtil.java
+++ Unit Management Service/src/main/java/com/electrogrid/util/DBUtil.java
@@ -13,7 +13,7 @@ public class DBUtil {
     private static Connection connection;
     public static final String URL = "jdbc:mysql://localhost:3306/unit_management_service?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "Atlaspitu80thanirule";
+    public static final String PASSWORD = "";
 
     public DBUtil() {
     }
