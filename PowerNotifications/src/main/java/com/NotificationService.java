d18a6ff3df02ded4dc644cd9154936be8f7b448b
Dineth Wijayasena <88510443+dinethWs@users.noreply.github.com>
2022-04-22 08:48:45 +0530

    NotifiCommit

--- PowerNotifications/src/main/java/com/NotificationService.java
+++ PowerNotifications/src/main/java/com/NotificationService.java
@@ -2,12 +2,6 @@ package com;
 import model.Notifications;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-//For JSON
-import com.google.gson.*;
-//For XML
-import org.jsoup.*;
-import org.jsoup.parser.*;
-import org.jsoup.nodes.Document;
 
 @Path("/Notifications")
 public class NotificationService {
