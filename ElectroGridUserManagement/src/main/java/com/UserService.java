f3fd0106cfe2c7e17ac20a293ea282d2a6289963
sashini00 <86517194+sashini00@users.noreply.github.com>
2022-04-24 11:44:54 +0530

    Update user service

--- ElectroGridUserManagement/src/main/java/com/UserService.java
+++ ElectroGridUserManagement/src/main/java/com/UserService.java
@@ -44,4 +44,23 @@ String output = userObj.insertUser(name, phoneNum, email, userName,password);
 return output;
 }
 
+@PUT
+@Path("/")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.TEXT_PLAIN)
+public String updateUser(String userData)
+{
+                  //Convert the input string to a JSON object
+                  JsonObject userObject = new JsonParser().parse(userData).getAsJsonObject();
+                  //Read the values from the JSON object
+                  String userID = userObject.get("userID").getAsString();
+                  String name = userObject.get("name").getAsString();
+                  String phoneNum = userObject.get("phoneNum").getAsString();
+                  String email = userObject.get("email").getAsString();
+                  String userName= userObject.get("userName").getAsString();
+                  String password= userObject.get("password").getAsString();
+                  String output = userObj.updateUser(userID, name, phoneNum, email, userName,password);
+return output;
+}
+
 }
