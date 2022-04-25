788f23f917060bc1d7621045a35951a464f75bbc
sashini00 <86517194+sashini00@users.noreply.github.com>
2022-04-25 16:50:19 +0530

    Delete user service

--- ElectroGridUserManagement/src/main/java/com/UserService.java
+++ ElectroGridUserManagement/src/main/java/com/UserService.java
@@ -63,4 +63,19 @@ public String updateUser(String userData)
 return output;
 }
 
+@DELETE
+@Path("/")
+@Consumes(MediaType.APPLICATION_XML)
+@Produces(MediaType.TEXT_PLAIN)
+public String deleteUser(String userData)
+{
+                  //Convert the input string to an XML document
+                  Document doc = Jsoup.parse(userData, "", Parser.xmlParser());
+                  //Read the value from the element <userID>
+                  String userID = doc.select("userID").text();
+                  String output = userObj.deleteUser(userID);
+ return output;
+}
+
+
 }
